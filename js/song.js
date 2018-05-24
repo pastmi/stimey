256c14979e29d39cfc748ae98b20ff38a61690d3
Гинько Михаил <accer96226@gmail.com>
2018-05-24 11:18:24 +0300

    song fix

--- js/song.js
+++ js/song.js
@@ -1,23 +1,23 @@
-// let play = true,
-//   soundBlock = document.querySelector(".sound"),
-//   song = document.querySelector(".toggleSong");
-// let toggleSound = () => {
-//   musicPlay('click');
-//   if (play) {
+let play = true,
+  soundBlock = document.querySelector(".sound"),
+  song = document.querySelector(".toggleSong");
+let toggleSound = () => {
+  musicPlay('click');
+  if (play) {
    
-//     soundBlock.pause();
-//     play=false;
-//     song.children[0].setAttribute("src", "./img/song2.png");
-//     return;
-//   }
-//   if (!play) {
+    soundBlock.pause();
+    play=false;
+    song.children[0].setAttribute("src", "./img/song2.png");
+    return;
+  }
+  if (!play) {
     
-//     soundBlock.play();
-//     play = true;
-//     song.children[0].setAttribute("src", "./img/song1.png");
-//     return;
-//   }
-//   soundBlock.stop();
-// };
+    soundBlock.play();
+    play = true;
+    song.children[0].setAttribute("src", "./img/song1.png");
+    return;
+  }
+  soundBlock.stop();
+};
 
-// song.addEventListener("click", toggleSound);
+song.addEventListener("click", toggleSound);
